5ff6be471a6d5667929c2131c7f2a1fa04a8c6e8
kairzhan8 <kairzhankural@gmail.com>
2020-12-10 11:20:45 +0600

    Initial Commit

---
+++ notificationsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notificationsTest
+//
+//  Created by Kairzhan Kural on 12/10/20.
+//  Copyright © 2020 Kairzhan Kural. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
